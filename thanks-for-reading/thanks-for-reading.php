8786a755380dacda448e8561a1c884ccfdd5d60b
Leo Koo <leokoo@gmail.com>
2016-10-15 04:17:40 +0800

    using filters with the thanks for reading plugin

---
+++ thanks-for-reading/thanks-for-reading.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Plugin Name: Thanks for Reading
+ * Description: Learning plugin development from hardcoreWP as part of the Pippin's plugin development 101
+ * Version: 1.0
+ * Author: Leo Koo
+ * Author URI: http://www.wpstarters.com
+ * License: GPL2
+ *
+ */
+
+
+
+ function tfr_the_content( $content ) {
+   return $content . '<p>Thanks for Reading my writing</p>';
+ }
+ add_filter( 'the_content', 'tfr_the_content' );
