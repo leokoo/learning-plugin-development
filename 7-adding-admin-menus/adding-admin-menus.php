d5e5d2d6c57b73884399cfc8c460f86d97e21fda
Leo Koo <leokoo@gmail.com>
2016-10-20 22:10:32 +0800

    adding submenu page and rendering it

--- 7-adding-admin-menus/adding-admin-menus.php
+++ 7-adding-admin-menus/adding-admin-menus.php
@@ -8,8 +8,17 @@ Version: 1.0
 Author URI: https://www.wpstarters.com
 */
 
+/*
+https://developer.wordpress.org/reference/functions/add_menu_page/
+add_menu_page( string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '', string $icon_url = '', int $position = null )
+
+https://developer.wordpress.org/reference/functions/add_submenu_page/
+add_submenu_page( string $parent_slug, string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '' )
+*/
+
 function pd101_add_menu_page() {
 	add_menu_page('Plugin Dev 101', 'PD101', 'edit_pages', 'pd101', 'pd101_render_admin', false, 63);
+	add_submenu_page('pd101', 'PD101 Settings', 'Settings', 'edit_pages', 'pd101-settings', 'pd101_render_settings');
 }
 add_action('admin_menu', 'pd101_add_menu_page');
 
@@ -18,5 +27,8 @@ function pd101_render_admin() {
 	echo 'You are now seeing our admin screen';
 }
 
+function pd101_render_settings() {
+	echo 'You are now seeing our submenu settings screen';
+}
 
 ?>
