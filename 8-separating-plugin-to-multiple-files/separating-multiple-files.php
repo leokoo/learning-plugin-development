dd30ca3d5c6b756d61f20e7df7d8676a5085d339
Leo Koo <leokoo@gmail.com>
2016-10-21 04:15:20 +0800

    conditionally loading certain files to improve performance

--- 8-separating-plugin-to-multiple-files/separating-multiple-files.php
+++ 8-separating-plugin-to-multiple-files/separating-multiple-files.php
@@ -10,8 +10,18 @@ Author URI: https://www.wpstarters.com
 
 //We define the absolute plugin path to avoid rare cases of conflict with other plugins that have files of the same name. At the same time, once we've defined the plugin path, we can just reuse it again and again
 define( 'PD101_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
-	include( PD101_PLUGIN_PATH . 'includes/admin-scripts.php');
-	include( PD101_PLUGIN_PATH . 'includes/scripts.php');
+
+/*
+	https://codex.wordpress.org/Function_Reference/is_admin
+	This Conditional Tag checks if the Dashboard or the administration panel is attempting to be displayed. It is a boolean function that will return true if the URL being accessed is in the admin section, or false for a front-end page.
+*/
+
+	if( is_admin() ){
+			include( PD101_PLUGIN_PATH . 'includes/admin-scripts.php');
+	}
+		else {
+			include( PD101_PLUGIN_PATH . 'includes/scripts.php');
+		}
 	include( PD101_PLUGIN_PATH . 'includes/post-type.php');
 
 ?>
