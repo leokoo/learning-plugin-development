f25cfa77d7aa40f0a667e2ea98c7bf53723cc0f8
Leo Koo <leokoo@gmail.com>
2016-10-21 13:33:49 +0800

    starting oop in WP plugins

---
+++ 9-oop-in-plugins/oop-in-plugins.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: 9-OOP in WordPress Plugins
+Plugin URI: https://github.com/leokoo/learning-plugin-development
+Description: We're learning how to implement simple PHP based OOP in the WordPress plugin without things like inheritance
+Author: Leo Koo
+Version: 1.0
+Author URI: https://www.wpstarters.com
+*/
+
+
+
+?>
