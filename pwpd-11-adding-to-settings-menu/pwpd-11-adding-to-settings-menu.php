918d5b89ef93d1bfa985f350b0a1b4cd606036c2
Leo Koo <leokoo@gmail.com>
2016-10-29 03:07:50 +0800

    e.g. settings submenu

---
+++ pwpd-11-adding-to-settings-menu/pwpd-11-adding-to-settings-menu.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: PWPD 11 - Creating a sub-menu under the settings menu
+Plugin URI: https://github.com/leokoo/learning-plugin-development
+Description: A simple plugin on creating a custom sub-menu on WordPress
+Author: Leo Koo
+Version: 1.0
+Author URI: https://www.wpstarters.com
+License: GPLv2
+*/
+
+add_action( 'admin_menu', 'boj_menuexample_create_menu' );
+
+function boj_menuexample_create_menu() {
+	
+	//create a submenu under Settings
+	add_options_page( 'Leo Plugin Settings Page', 'Menu Example Settings', 'manage_options', 'settings-submenu', 'boj_menuexample_settings_page' );
+	
+}
+
+?>
